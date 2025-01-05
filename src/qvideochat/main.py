47f63859af38ba338e51c689a4fef43aef3e7968
doas root <petr.zakharov.anton@gmail.com>
2025-01-06 00:32:42 +0300

    feat(videochat): create & compile app design

--- src/qvideochat/main.py
+++ src/qvideochat/main.py
@@ -1,11 +1,18 @@
 import sys
 
 import PyQt6.QtWidgets
+from PyQt6 import QtCore, QtWidgets
 
 from qvideochat.ui.videochat import Videochat
 
 
 def main() -> None:
+    if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+
+    if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
+        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+
     app = PyQt6.QtWidgets.QApplication(sys.argv)
     videochat = Videochat()
     videochat.show()
--- src/qvideochat/ui/videochat.py
+++ src/qvideochat/ui/videochat.py
@@ -1,25 +1,8 @@
 import PyQt6.QtWidgets
+from qvideochat.ui.gen.videochat import Ui_QVideoChat
 
 
-class Videochat(PyQt6.QtWidgets.QMainWindow):
-    def __init__(self) -> None:
+class Videochat(PyQt6.QtWidgets.QWidget, Ui_QVideoChat):
+    def __init__(self):
         super().__init__()
-        self.setWindowTitle('Videochat')
-
-        self.central_widget = PyQt6.QtWidgets.QWidget()
-        self.layout = PyQt6.QtWidgets.QVBoxLayout()
-
-        self.ping_button = PyQt6.QtWidgets.QPushButton('Ping')
-        self.event_list = PyQt6.QtWidgets.QListWidget
-
-        self.setup_ui()
-        self.setup_events()
-
-    def setup_ui(self) -> None:
-        self.setCentralWidget(self.central_widget)
-        self.central_widget.setLayout(self.layout)
-
-        self.layout.addWidget(self.ping_button)
-
-    def setup_events(self) -> None:
-        self.ping_button.clicked.connect(lambda: print('Ping!'))  # noqa
+        self.setupUi(self)

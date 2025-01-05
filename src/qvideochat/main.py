26ec0bd735f6c526d0e5811c7752bfc776d83ce7
doas root <petr.zakharov.anton@gmail.com>
2025-01-06 02:02:20 +0300

    feat(): create Config using pydantic

--- src/qvideochat/main.py
+++ src/qvideochat/main.py
@@ -1,18 +1,24 @@
 import asyncio
 import sys
 
-from PyQt6 import QtCore, QtWidgets
-from qasync import QEventLoop, QApplication
+from PyQt6 import QtCore
+from PyQt6 import QtWidgets
+from qasync import QApplication
+from qasync import QEventLoop
 
 from qvideochat.ui.videochat import Videochat
 
 
 def main() -> None:
     if hasattr(QtCore.Qt, 'AA_EnableHighDpiScaling'):
-        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_EnableHighDpiScaling, True)
+        QtWidgets.QApplication.setAttribute(
+            QtCore.Qt.AA_EnableHighDpiScaling, on=True,
+        )
 
     if hasattr(QtCore.Qt, 'AA_UseHighDpiPixmaps'):
-        QtWidgets.QApplication.setAttribute(QtCore.Qt.AA_UseHighDpiPixmaps, True)
+        QtWidgets.QApplication.setAttribute(
+            QtCore.Qt.AA_UseHighDpiPixmaps, on=True,
+        )
 
     app = QApplication(sys.argv)
 
@@ -30,5 +36,7 @@ def main() -> None:
 
 
 if __name__ == '__main__':
-    sys.excepthook = lambda exctype, value, traceback: sys.__excepthook__(exctype, value, traceback)
+    sys.excepthook = lambda exctype, value, traceback: sys.__excepthook__(
+        exctype, value, traceback,
+    )
     main()
--- src/qvideochat/ui/videochat.py
+++ src/qvideochat/ui/videochat.py
@@ -1,11 +1,9 @@
-import asyncio
-
 import PyQt6.QtWidgets
+
 from qvideochat.ui.gen.videochat import Ui_QVideoChat
-from qasync import asyncSlot
 
 
 class Videochat(PyQt6.QtWidgets.QWidget, Ui_QVideoChat):
-    def __init__(self):
+    def __init__(self) -> None:
         super().__init__()
         self.setupUi(self)
